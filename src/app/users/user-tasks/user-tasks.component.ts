ef53eb86fd078e062c858406ccb1311726026353
lucaslopescunha <lucaslopescunha@gmail.com>
2026-03-05 18:08:26 -0300

    Alternative way of getting username, using observables on paramMap.

--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -1,4 +1,5 @@
-import { Component, computed, inject, input } from '@angular/core';
+import { Component, inject, input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { UsersService } from '../users.service';
 
 @Component({
@@ -7,10 +8,23 @@ import { UsersService } from '../users.service';
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent {
+export class UserTasksComponent implements OnInit {
   userId = input.required<string>();
   private usersService = inject(UsersService);
+  private activatedRoute = inject(ActivatedRoute);
 
-  userName = computed(
+  /*userName = computed(
     () => this.usersService.users.find(u => u.id === this.userId())?.name);
+*/
+  userName = '';
+  ngOnInit(): void {
+    console.log(this.activatedRoute);
+    this.activatedRoute.paramMap.subscribe({
+      // paramMap is key/value pair: :userId -> value
+      next: paramMap => {
+        this.userName = this.usersService.users.find( u => u.id === paramMap.get('userId'))?.name || '';
+        console.log(paramMap.get('userId'));
+      }
+    })
+  }
 }
