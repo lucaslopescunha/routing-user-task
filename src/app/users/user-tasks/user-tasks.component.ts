4e5f63aa4c9246d391b470abb3669155bb82c3d3
lucaslopescunha <lucaslopescunha@gmail.com>
2026-03-05 17:55:10 -0300

    Loading username. Routing active link.

--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
+import { UsersService } from '../users.service';
 
 @Component({
   selector: 'app-user-tasks',
@@ -7,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './user-tasks.component.css',
 })
 export class UserTasksComponent {
+  userId = input.required<string>();
+  private usersService = inject(UsersService);
 
+  userName = computed(
+    () => this.usersService.users.find(u => u.id === this.userId())?.name);
 }
--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,15 +1,15 @@
 import { Component, computed, input } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { RouterLink, RouterLinkActive } from "@angular/router";
 import { User } from './user.model';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
 })
 export class UserComponent {
   user = input.required<User>();
 
-  imagePath = computed(() => 'users/' + this.user().avatar);
+  imagePath = computed(() => `users/${this.user().avatar}`);
 }
