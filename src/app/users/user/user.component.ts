665d02189cb2568dba8891dc135fd6337b1f53fc
lucaslopescunha <lucaslopescunha@gmail.com>
2026-03-05 15:51:45 -0300

    Using router link will only render the appropriate element.

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,10 +1,12 @@
 import { Component, computed, input } from '@angular/core';
+import { RouterLink } from "@angular/router";
 import { User } from './user.model';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
+  imports: [RouterLink],
 })
 export class UserComponent {
   user = input.required<User>();
