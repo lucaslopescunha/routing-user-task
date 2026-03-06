11068e4a2825ecedbac3779bacccd80cf7533f56
lucaslopescunha <lucaslopescunha@gmail.com>
2026-03-06 10:21:29 -0300

    Loading tasks by user. Necessary pass       paramsInheritanceStrategy: 'always' in app.config.ts .

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,6 +1,6 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
-import { Task } from './task/task.model';
+import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
@@ -10,5 +10,8 @@ import { Task } from './task/task.model';
 })
 export class TasksComponent {
   userId = input.required<string>();
-  userTasks: Task[] = [];
+  private tasksService = inject(TasksService);
+  userTasks = computed(() => 
+      this.tasksService.allTasks().filter((task) => task.userId === this.userId())
+  );
 }
