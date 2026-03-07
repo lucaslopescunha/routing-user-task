dbb689c75419cdc67acdff2f0cdb2936e486ee53
lucaslopescunha <lucaslopescunha@gmail.com>
2026-03-07 12:52:14 -0300

    Using rule to unauthorize a page. Good to use with roles. canMatch clause.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,18 @@
-import { Routes } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanMatchFn, RedirectCommand, Router, Routes } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { resolveTitle, resolveUserName, UserTasksComponent } from './users/user-tasks/user-tasks.component';
 import { userRoutes } from './users/users.routes';
+
+const dummyCanMatch: CanMatchFn = (route, segments) => {
+    const shouldGetAccess = Math.random();
+    const router = inject(Router);
+    if(shouldGetAccess < 0.5) {
+        return true;
+    } 
+    return new RedirectCommand(router.parseUrl('/unauthorized'));
+}
 
 export const routes: Routes = [
     {
@@ -14,6 +24,7 @@ export const routes: Routes = [
         path: 'users/:userId',
         component: UserTasksComponent,
         children: userRoutes,
+        canMatch: [dummyCanMatch],
         data: {
             /** This will be a message input to UserTasksComponent.
              * With withComponentInputBinding enabled on app.config.ts
